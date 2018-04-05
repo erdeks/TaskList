7ca30030833a3963e24442daf8e419cc6e7e36ae
erdeks <aponcelasgarcia@iesesteveterradas.cat>
2018-04-05 17:21:41 +0200

    demo

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
 $app->get('/cowsay', function() use($app) {
   $app['monolog']->addDebug('cowsay');
   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
